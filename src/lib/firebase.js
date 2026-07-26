0574e6be24d380bee3764b8dcb41fdd83613a6a3
sosgkdl95-code4848 <sosgkdl95-code4848@users.noreply.github.com>
2026-07-26 10:32:31 +0900

    Update firebase.js with measurementId support

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Next.js HMR(Hot Module Replacement) 환경에서 중복 초기화 방지
